acea4e8d8a2a2abd354cef0978e9719a5b81d1e2
Giuseppetm <gdeambo@yahoo.it>
2022-04-18 11:22:39 +0200

    Added some buttons

--- src/Components/SectionOne.tsx
+++ src/Components/SectionOne.tsx
@@ -1,4 +1,4 @@
-import { Title, Text, Container, Grid, Image } from '@mantine/core';
+import { Title, Text, Container, Grid, Image, Button } from '@mantine/core';
 
 const SectionOne = () => {
     //const theme = useMantineTheme();
@@ -8,11 +8,14 @@ const SectionOne = () => {
             <Container>
                 <Grid justify="space-around">
                     <Grid.Col xs={6} sm={8} md={8} lg={8}>
-                        <Text color="black">
-                            <Title order={1}>You can put whatever you want here</Title>
-                            Dennis Farina was one of Hollywood's busiest actors and a familiar face to moviegoers and television viewers alike.
-                            In 1998's "Saving Private Ryan," directed by Steven Spielberg, Farina played "Col. Anderson," a pivotal role in the film.
-                        </Text>
+                        <div style={{ marginBottom: 20 }}>
+                            <Text color="black">
+                                <Title order={1}>You can put whatever you want here</Title>
+                                Dennis Farina was one of Hollywood's busiest actors and a familiar face to moviegoers and television viewers alike.
+                                In 1998's "Saving Private Ryan," directed by Steven Spielberg, Farina played "Col. Anderson," a pivotal role in the film.
+                            </Text>
+                        </div>
+                        <Button>Check it out</Button>
                     </Grid.Col>
                     <Grid.Col xs={6} sm={4} md={4} lg={4}>
                         <Image src={require('../Images/sample1.png')} alt={'sample1'} style={{ width: '100%', maxWidth: 300 }} />
--- src/Components/SectionTwo.tsx
+++ src/Components/SectionTwo.tsx
@@ -1,4 +1,4 @@
-import { Grid, Text, Container, Title, Image } from '@mantine/core';
+import { Grid, Text, Container, Title, Image, Button } from '@mantine/core';
 
 const SectionTwo = () => {
     //const theme = useMantineTheme();
@@ -11,13 +11,14 @@ const SectionTwo = () => {
                         <Image src={require('../Images/sample2.png')} alt={'sample2'} style={{ width: '100%', maxWidth: 300 }} />
                     </Grid.Col>
                     <Grid.Col xs={6} sm={8} md={8} lg={8}>
-                        <div>
+                        <div style={{ marginBottom: 20 }}>
                             <Text color="black">
                                 <Title order={1}>Put something here too</Title>
                                 Dennis Farina is unique among thespians in that he was one of the few to achieve success as a "late-bloomer." 
                                 He did not start acting until he was 37 years old, after stints in the military and 18 years on the Chicago Police Department.
                             </Text>
                         </div>
+                        <Button>Tell me more</Button>
                     </Grid.Col>
                 </Grid>
             </Container>
